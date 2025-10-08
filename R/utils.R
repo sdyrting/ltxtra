7b46ae11c33405921b96e5ddcfc08c35ec3d3304
Sigurd Dyrting <sigurd.dyrting@cdu.edu.au>
2025-10-08 09:57:38 +0930

    Fixed bugs in ogompertz and add DESCRIIPTION file

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,29 @@
 # Add functions that other functions might use
+#' Calibrate Gompertz model to last two age intervals of a life table
+#'
+#' @param q Death probability for the last closed interval
+#' @param n Age interval for the last closed interval
+#' @param olifeexp Life expectancy at the start of the open interval
+#'
+#' @returns A list of Gompertz parameters
+#' @importFrom expint expint
+#' @export
+#'
+#' @examples
+#' ogompertz(0.3,5,10)
 ogompertz <- function(q,n,olifeexp) {
   
   MAXITS <- 20
   RELERR <- 1.0e-6
   
   dx= -n
+  
+  ans <- list(Ro=NA,
+              ao=NA,
+              co=NA,
+              bo=NA,
+              errflag=TRUE,
+              errmsg='ogompertz: Undefined result')
   
   alpha <- -log(1-q);
   beta <- olifeexp
@@ -19,9 +38,9 @@ ogompertz <- function(q,n,olifeexp) {
     ans$Ro <- 1.0/beta
     ans$ao <- 0.0
     ans$co <- 1.0
-    ans$bo <- INF
+    ans$bo <- Inf
     ans$errflag <- FALSE
-    and$errmsg <- ''
+    ans$errmsg <- ''
     return(ans)
   }
   
@@ -31,7 +50,7 @@ ogompertz <- function(q,n,olifeexp) {
   for (j in 1:5) {#Iterate initial guess
     xa <- exp(a*dx)
     theta <- alpha/(1.0-xa)
-    e1 <- expint(theta,scale=TRUE)
+    e1 <- expint::expint(theta,scale=TRUE)
     a <- e1/beta
   }
   
@@ -40,7 +59,7 @@ ogompertz <- function(q,n,olifeexp) {
     xa <- exp(a*dx)
     theta <- alpha/(1.0-xa)
     dtheta <- theta*(xa/(1.0-xa))*dx
-    e1 <- expint(theta,scale=TRUE)
+    e1 <- expint::expint(theta,scale=TRUE)
     de1 <- dtheta*(e1-1.0/theta)
     f <- e1/a-beta
     df <- (de1*a-e1)/(a*a)
@@ -56,13 +75,13 @@ ogompertz <- function(q,n,olifeexp) {
       ans$ao <- a
       ans$Ro <- theta*a
       ans$errflag <- FALSE
-      and$errmsg <- ''
+      ans$errmsg <- ''
       return(ans)
     }
   }
   
   ans$errflag <- TRUE
-  ans$errmsg <- "MAXITS exceeded"
+  ans$errmsg <- "ogompertz: MAXITS exceeded"
   
   return(ans)
 }
